2cffec303208e9fada4000dcf7f9fd81dc148df3
fernando.goia <fernando.goia@acqio.com.br>
2025-11-27 15:09:18 -0300

    chore: implementing basic structure of task form modal

--- 01-go-task/src/app/app.ts
+++ 01-go-task/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { MainContent } from './components/main-content/main-content';
+import { TaskFormModal } from "./components/task-form-modal/task-form-modal";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, MainContent],
+  imports: [Header, MainContent, TaskFormModal],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

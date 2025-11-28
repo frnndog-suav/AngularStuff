b04f8e624e308522e237c7faebee2a61dcc4bda1
Fernando <frnnd.goia97@gmail.com>
2025-11-28 12:38:57 -0300

    chore: implementing comentary modal layout

--- 01-go-task/src/app/app.ts
+++ 01-go-task/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { MainContent } from './components/main-content/main-content';
-import { TaskFormModal } from "./components/task-form-modal/task-form-modal";
+import { TaskCommentsModal } from './components/task-comments-modal/task-comments-modal';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, MainContent, TaskFormModal],
+  imports: [Header, MainContent, TaskCommentsModal],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

7d2cac9a646a59670d3f82be618f733f5ee33569
fernando.goia <fernando.goia@acqio.com.br>
2025-12-01 11:16:26 -0300

    refactor (task service): cloning tasks list to avoid mutation

--- 01-go-task/src/app/services/task.service.ts
+++ 01-go-task/src/app/services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { TASK_STATUS } from '../enums/task-status';
 import { TTask } from '../type/task';
 import { TTaskFormControls } from '../type/task-form-controls';
@@ -10,11 +10,13 @@ import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-
 })
 export class TaskService {
   private todoTasks$ = new BehaviorSubject<TTask[]>([]);
-  readonly todoTasks = this.todoTasks$.asObservable();
+  readonly todoTasks = this.todoTasks$.asObservable().pipe(map((tasks) => structuredClone(tasks)));
   private doingTasks$ = new BehaviorSubject<TTask[]>([]);
-  readonly doingTasks = this.doingTasks$.asObservable();
+  readonly doingTasks = this.doingTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
   private doneTasks$ = new BehaviorSubject<TTask[]>([]);
-  readonly doneTasks = this.doneTasks$.asObservable();
+  readonly doneTasks = this.doneTasks$.asObservable().pipe(map((tasks) => structuredClone(tasks)));
 
   addTask(task: TTaskFormControls): void {
     const newTask: TTask = {

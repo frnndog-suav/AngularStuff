20a0a44673f82f01199c3724bd7b7a41aa3e16e1
fernando.goia <fernando.goia@acqio.com.br>
2025-12-02 14:07:53 -0300

    chore (task list comment): implementing form control to task comment

--- 01-go-task/src/app/components/task-comments-modal/task-comments-modal.ts
+++ 01-go-task/src/app/components/task-comments-modal/task-comments-modal.ts
@@ -1,13 +1,20 @@
 import { DIALOG_DATA } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TTask } from '../../type/task';
 
 @Component({
   selector: 'app-task-comments-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-comments-modal.html',
   styleUrl: './task-comments-modal.css',
 })
 export class TaskCommentsModal {
+  commentControl = new FormControl('', [Validators.required]);
+
   readonly _task: TTask = inject(DIALOG_DATA);
+
+  onAddComment() {
+    console.log('Comentário:', this.commentControl.value);
+  }
 }
